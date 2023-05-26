1a305533e87d1632115d46a4ab64a8ff25efbc33
Craig Russell <ctr26@ebi.ac.uk>
2023-05-26 10:50:40 +0100

    fixing biostudies link

--- biostudies/get_files.sh
+++ biostudies/get_files.sh
@@ -1,2 +1,2 @@
-wget https://www.ebi.ac.uk/biostudies/files/S-BSST666/ZeroCostDl4Mic/Stardist_v2
+wget https://www.ebi.ac.uk/biostudies/files/S-BSST666/ZeroCostDl4Mic/Stardist_v2.zip
 unzip Stardist_v2
